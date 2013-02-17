a155fc904095e0b58deef56783bdf3d1d7c5242f
Steve Armstrong <sarmstrongmusic@gmail.com>
2013-02-16 19:42:23 -0500

    Changed syntax of template tags to more closely mirror WP template tags, added PHPDoc syntax

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  * 
  */
 
-/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : sarmstrongmusic@gmail.com)
+/*  Copyright 2012  Steve Armstrong (email : sarmstrongmusic@gmail.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as
@@ -28,77 +28,124 @@
 
  */
 
+/**
+ * class CMT (Custom Meta Tags)
+ * 
+ * Allows you to use your post meta as a template tag simliar to the WordPress syntax the_title(). 
+ * If you have meta data in a cusomt field called 'artist', you could call this in you template with the
+ * following. CMT::the_artist(). Or, to return a value, you could use CMT::get_the_artist(). 
+ * Additional convenience function meta_exists() to check if post meta exists or not.
+ * 
+ */
 class CMT {
 
-    public static function __callStatic($method, $args) {
+     /**
+      * __callStatic
+      * 
+      * If you have post meta that has a key of 'artist', you can retrieve the meta by calling the_artists(), 
+      * or get_the_artists().
+      * 
+      * @param string $method name of you custom meta tag the_artist()
+      * @param array $args Leave empty, required by _callStatic
+      */
+     public static function __callStatic($method, $args) {
 
-        if (substr($method, 0, 4) === "the_") {
+          if (substr($method, 0, 4) === "the_") {
 
-            echo get_post_meta(get_the_ID(), substr($method, 4), true);
-            
-        } else if (substr($method, 0, 8) === "get_the_") {
+               echo get_post_meta(get_the_ID(), substr($method, 4), true);
+               
+          } else if (substr($method, 0, 8) === "get_the_") {
 
-            return get_post_meta(get_the_ID(), substr($method, 8), true);
-            
-        } else if (substr($method, -7) === "_exists") {
+               return get_post_meta(get_the_ID(), substr($method, 8), true);
+               
+          } else if (substr($method, -7) === "_exists") {
 
-            return get_post_meta(get_the_ID(), substr($method, 0, -7), true) === "" ? false : true;
-            
-        }
-    }
+               return get_post_meta(get_the_ID(), substr($method, 0, -7), true) === "" ? false : true;
+               
+          } else {
+               
+               return false;
+          }
+     }
 
 }
 
+/**
+ * class CMT (Custom Taxonomy Tags)
+ * 
+ * Allows you to use custom taxonomys a template tags simliar to the WordPress syntax the_tags(). 
+ * If you have a custom taxonomy called skill', you could call this in you template with the
+ * following. CMT::the_artist(). Or, to return a value, you could use CMT::get_the_artist(). 
+ * Additional convenience function tag_exists() to check if tag exists or not.
+ * 
+ * 
+ */
 class CTT {
 
-    public static function __callStatic($method, $args) {
+     /**
+      * __callStatic
+      * 
+      * If you have post meta that has a key of 'artist', you can retrieve the taxonomy by calling the_artists(), 
+      * or get_the_artists().
+      * 
+      * @param string $method name of you custom taxonomony tag
+      * @param array $args pass in before, seperator and after paramters like the_tags(), use get_the_tags() similarly
+      */
+     public static function __callStatic($method, $args) {
 
-        if (substr($method, 0, 4) == 'the_') {
-            
-            $term = substr($method, 4); 
-            
-            $list = self::handle_terms($term ,$args); 
+          if (substr($method, 0, 4) == 'the_') {
 
-            echo gettype($list) == "string" ? $list : ''; 
-            
-        } else if (substr($method, 0, 8) === "get_the_") {
-            
-            $term = substr($method, 8); 
-            
-            $list = self::handle_terms($term ,$args); 
-            
-            return gettype($list) == "string" ? $list : '';   
-            
-        } else if (substr($method, -7) === "_exists") {
+               $term = substr($method, 4);
 
-            $obj  = get_the_terms( get_the_ID() , substr($method, 0, -7) ); 
-            
-            return $obj && !is_wp_error($obj) ? true : false;  
-        }
-    }
+               $list = self::handle_terms($term, $args);
 
-    private static function handle_terms($term , $args) {
+               echo gettype($list) == "string" ? $list : '';
+               
+          } else if (substr($method, 0, 8) === "get_the_") {
 
-        isset($args[0]) ? $seperator = $args[0] : $seperator = false;
-        
-        if ($seperator == false) {
+               $term = substr($method, 8);
+               
+               if (count($args) === 0) {
+                    
+                    $terms = get_the_terms(get_the_ID() , $term);
+                    
+               } else {
+                    
+                    
+                    $terms = get_the_terms($args[0] , $term);
+               }
 
-            $str = '<ul class="post-' . $term . '" >';
+               return $terms && !is_wp_error($terms) ? $terms : NULL;
+               
+          } else if (substr($method, -7) === "_exists") {
 
-            $str .= get_the_term_list(get_the_ID(), $term, '<li>', "</li><li>", "</li>");
+               $obj = get_the_terms(get_the_ID(), substr($method, 0, -7));
 
-            $str .= "</ul>";
+               return $obj && !is_wp_error($obj) ? true : false;
+               
+          } else {
+               
+               return false;
+          }
+          
+          
+     }
 
-            return $str;
-            
-        } else {
-            
-            $str = get_the_term_list(get_the_ID(), $term, '', $seperator , '');
-            
-            return $str; 
-            
-        }
-    }
+     private static function handle_terms($term, $args) {
+          
+          $func_params = array(); 
+          
+          array_push($func_params , get_the_ID()); 
+          
+          array_push($func_params , $term); 
+          
+          $func_params = array_merge($func_params , $args); 
+          
+          $str = call_user_func_array('get_the_term_list', $func_params);
+
+          return $str;
+          
+     }
 
 }
 
